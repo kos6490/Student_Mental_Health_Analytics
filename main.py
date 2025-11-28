15a946d0174ec85a0962707a807f94f22cb76569
kos6490 <kos-6490@naver.com>
2025-11-28 19:06:56 +0900

    feat: Add one data analysis item and modularize files

--- main.py
+++ main.py
@@ -4,8 +4,32 @@
 # File load check
 
 import pandas as pd
+from src import data_processing
+from src import data_analysis
 
-data_file = pd.read_csv('student_mental_health.csv')
+# 데이터 로딩
+data_file = pd.read_csv("data/student_mental_health.csv")
 
-print(data_file.head())
-print(data_file.info()) 
+# 데이터 전처리
+data_file = data_processing.processing(data_file)
+
+# 데이터 분석
+print("[학년과 우울증/불안의 관계]")
+print(data_analysis.analyze_depression_anxiety_by_year(data_file))
+print()
+
+print("[성적과 공황발작의 관계]")
+print(data_analysis.analyze_panic_by_cgpa(data_file))
+print()
+
+print("[성적과 우울증의 관계]")
+print(data_analysis.analyze_depression_by_cgpa(data_file))
+print()
+
+print("[전공과 불안의 관계]")
+print(data_analysis.analyze_anxiety_by_course(data_file))
+print()
+
+print("[공황발작과 치료 받은 비율의 관계]")
+print(data_analysis.analyze_treatment_by_panic(data_file))
+print()
